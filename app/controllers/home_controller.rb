a990cfaac1935066e7799faa47389f7a930fa58e
Unknown <james@scrunchup.com>
2017-11-02 12:26:23 +0800

    Tidied up code in results controller. Added defaults.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,47 +5,29 @@ class HomeController < ApplicationController
 
   def results
 
-    if params.has_key?(:results)
+    if params.has_key?(:@results)
+
+       @resultsparams = params[:@results]
+
+    elsif params.has_key?(:results)
+
+      @resultsparams = params[:results]
+
+    end
 
     #@results = Venue.joins(:offers).where("offers.offertype = '2-4-1'")
 
-    @finddate = (params[:results][:date]).to_date
-    @findtime = (params[:results][:time]).to_time
+    @finddate = (@resultsparams[:date]).to_date
+    @findtime = (@resultsparams[:time]).to_time
     @resultsdate = DateTime.new(@finddate.year,@finddate.month,@finddate.day,@findtime.hour)
 
-    if params[:results][:vtype] == 'Anywhere'
+    if @resultsparams[:vtype] == 'Anywhere'
 
     @results = Venue.joins(:offers).where(["DATE(offers.start) = ? and EXTRACT(hour FROM offers.end) >= ?", @finddate , @findtime.strftime('%H')])
 
     else
 
-    @results = Venue.joins(:offers).where(["venues.vtype = ? and DATE(offers.start) = ? and EXTRACT(hour FROM offers.end) >= ?", params[:results][:vtype], @finddate , @findtime.strftime('%H')])
-
-    end
-
-  elsif params.has_key?(:@results)
-
-    @finddate = (params[:@results][:date]).to_date
-    @findtime = (params[:@results][:time]).to_time
-    @resultsdate = DateTime.new(@finddate.year,@finddate.month,@finddate.day,@findtime.hour)
-
-    if params[:@results][:vtype] == 'Anywhere'
-
-    @results = Venue.joins(:offers).where(["DATE(offers.start) = ? and EXTRACT(hour FROM offers.end) >= ?", @finddate , @findtime.strftime('%H')])
-
-
-    else
-
-      @results = Venue.joins(:offers).where(["venues.vtype = ? and DATE(offers.start) = ? and EXTRACT(hour FROM offers.end) >= ?", params[:@results][:vtype], @finddate , @findtime.strftime('%H')])
-
-    end
-
-
-  else
-
-    @results = Venue.joins(:offers).where(["offers.start = ?", Date.today])
-
-    #@results = Venue.joins(:offers).where("offers.offertype = '2-4-1'")
+    @results = Venue.joins(:offers).where(["venues.vtype = ? and DATE(offers.start) = ? and EXTRACT(hour FROM offers.end) >= ?", @resultsparams[:vtype], @finddate , @findtime.strftime('%H')])
 
     end
 
